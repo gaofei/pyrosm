4f12afe57dea795548d5caeaacc1c0482628c586
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-07-01 19:19:04 +0100

    Check if geopandas version is above 0.8.0

--- pyrosm/utils/_compat.py
+++ pyrosm/utils/_compat.py
@@ -1,6 +1,18 @@
 from shapely.geos import geos_version_string as shapely_geos_version
 from pygeos import geos_capi_version_string
 import warnings
+from distutils.version import LooseVersion
+import geopandas as gpd
+
+# ===============================
+# Version info
+# ===============================
+
+GEOPANDAS_GE_080 = str(gpd.__version__) >= LooseVersion("0.8.0")
+
+# ===============================
+# PYGEOS / Shapely compat
+# ===============================
 
 # shapely has something like: "3.6.2-CAPI-1.10.2 4d2925d6"
 # pygeos has something like: "3.6.2-CAPI-1.10.2"
